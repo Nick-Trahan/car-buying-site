acf2c06338cfb0bf7ffa08a4f5fcf25146a2c288
Nick Trahan <43890781+Nick-Trahan@users.noreply.github.com>
2018-12-13 14:52:19 -0600

    Create, style, and script go button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//JS goes here
+let getStartedButton = document.querySelector('#get-started');
+let questionBox = document.querySelector('#question-box');
+let bodyTitle = document.querySelector('#body-title');
+let introText = document.querySelector('#intro-text');
+
+getStartedButton.addEventListener('click', askQuestions);
+
+function askQuestions() {
+  getStartedButton.style.display = 'none';
+  introText.style.display = 'none';
+  bodyTitle.innerHTML = 'This is a test. IT WORKED!';
+  questionBox.innerHTML = 'Lots of words go here.';
+
+}
